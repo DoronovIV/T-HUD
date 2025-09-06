9ffbb99b62e5744684607699a547dc90cae4ca7d
DoronovIV <ipv2743@gmail.com>
2025-09-07 01:18:06 +0300

    Produce test profile

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,6 +1,7 @@
 import type { Observable } from 'npm:rxjs';
+import type { Config } from '../model/config.type.ts';
 import { fileContents$ } from './basic/file.service.ts';
 
-const testConfigPath = './src/assets/profile.json';
+const testConfigPath = './src/config.json';
 
-export const profile$: Observable<object> = fileContents$(testConfigPath);
+export const profile$: Observable<Config> = fileContents$(testConfigPath);
